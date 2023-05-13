eb24bedacb9016dc109f369c340cb242f69fd23b
Codescandy <codescandy@192.168.1.3>
2023-05-13 16:24:25 +0530

    initiate vibe UI on github

---
+++ src/asstes/js/main.js
@@ -0,0 +1,11 @@
+ 
+
+// Import all of prism's JS
+import "../../../node_modules/prismjs/prism";
+import "../../../node_modules/prismjs/plugins/toolbar/prism-toolbar";
+import "../../../node_modules/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
+// Main CSS 
+
+import "../../../node_modules/prismjs/plugins/toolbar/prism-toolbar.css"
+import "../../asstes/css/main.css";
+
---
+++ vite.config.js
@@ -0,0 +1,57 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import glob from "glob";
+import path from "path";
+import handlebars from 'vite-plugin-handlebars';
+
+ 
+export default defineConfig({
+  // ...
+  base: './',
+  root: path.resolve(__dirname, 'src'),
+
+  // Server https://vitejs.dev/config/server-options.html
+  server:{
+    host:true,
+    port:3000, // you can specify which IP addresses the server should listen on.
+    hot: true
+  },
+
+  build:{
+    outDir: "../dist",
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: glob.sync(path.resolve(__dirname, "src", "*.html")),
+      output: {
+          chunkFileNames: 'assets/js/[name].js',
+          entryFileNames: 'assets/js/[name].js',
+          
+          assetFileNames: ({name}) => {
+            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+                return 'assets/images/[name][extname]';
+            }
+            
+            if (/\.css$/.test(name ?? '')) {
+                return 'assets/css/[name][extname]';   
+            }
+   
+            // default value
+            // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+            return 'assets/[name][extname]';
+          },
+        },
+        watch: {
+          exclude: 'src/partials/**'
+        },
+        
+      }
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: path.resolve(__dirname, "src", 'partials', ),
+      // reloadOnPartialChange: true,
+      
+    }),
+  ],
+})
